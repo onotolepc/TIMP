5cb3e41fbdb066e314c5d9cbba732d7152c0a014
onotolepc <118989101+onotolepc@users.noreply.github.com>
2022-11-24 22:06:46 +0700

    Add files via upload

--- modules.h
+++ modules.h
@@ -10,15 +10,10 @@ class Movie {
 public:
 	static Movie* MovieIn(std::ifstream& _inputStream);
 	std::string _name;
-	std::string _country;
 	Movie();
 	~Movie();
-	unsigned int countOfVowels();
-	virtual bool input(std::ifstream& _inputStream);
+	virtual void input(std::ifstream& _inputStream);
 	virtual void output(std::ofstream& _outputStream);
-	virtual void skipCartoon(std::ofstream& _outputStream);
-	virtual void skipGaming(std::ofstream& _outputStream);
-	virtual void skipDocument(std::ofstream& _outputStream);
 };
 
 class Gaming : public Movie {
@@ -27,11 +22,8 @@ private:
 public:
 	Gaming();
 	~Gaming();
-	bool input(std::ifstream& _inputStream);
+	void input(std::ifstream& _inputStream);
 	void output(std::ofstream& _outputStream);
-	void skipCartoon(std::ofstream& _outputStream);
-	void skipGaming(std::ofstream& _outputStream);
-	void skipDocument(std::ofstream& _outputStream);;
 };
 
 class Cartoon : public Movie {
@@ -47,11 +39,8 @@ private:
 public:
 	Cartoon();
 	~Cartoon();
-	bool input(std::ifstream& _inputStream);
+	void input(std::ifstream& _inputStream);
 	void output(std::ofstream& _outputStream);
-	void skipCartoon(std::ofstream& _outputStream);
-	void skipGaming(std::ofstream& _outputStream);
-	void skipDocument(std::ofstream& _outputStream);
 };
 
 class Document : public Movie {
@@ -60,18 +49,15 @@ private:
 public:
 	Document();
 	~Document();
-	bool input(std::ifstream& _inputStream);
+	void input(std::ifstream& _inputStream);
 	void output(std::ofstream& _outputStream);
-	void skipCartoon(std::ofstream& _outputStream);
-	void skipGaming(std::ofstream& _outputStream);
-	void skipDocument(std::ofstream& _outputStream);
 };
+
 
 class Container {
 public:
 	Container();
 	~Container();
-	void sort();
 	void input(std::ifstream& _inputStream);
 	void pushback(Movie* _inputMovie);
 	void print(std::ofstream& _outputStream);
@@ -86,8 +72,6 @@ private:
 		ContainerNode* _prev;
 	};
 	ContainerNode* _head;
-	bool compare(ContainerNode* _first, ContainerNode* _second);
 };
-
 #endif // !__MODULES__
 
