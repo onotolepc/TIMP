295865d61be12022fe922346c9d8690e9aa8bc01
onotolepc <118989101+onotolepc@users.noreply.github.com>
2022-11-25 00:27:39 +0700

    Add files via upload

--- modules.h
+++ modules.h
@@ -5,63 +5,34 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <regex>
 
-class Movie {
-public:
-	static Movie* MovieIn(std::ifstream& _inputStream);
-	std::string _name;
-	Movie();
-	~Movie();
-	unsigned int countOfVowels();
-	virtual void input(std::ifstream& _inputStream);
-	virtual void output(std::ofstream& _outputStream);
-};
+struct Gaming;
+void GamingDelete(Gaming& _gaming);
+void GamingInput(Gaming& _gaming, std::ifstream &_inputStream);
+void GamingOutput(Gaming& _gaming, std::ofstream &_outputStream);
 
-class Gaming : public Movie {
-private:
-	char* _director;
-public:
-	Gaming();
-	~Gaming();
-	void input(std::ifstream& _inputStream);
-	void output(std::ofstream& _outputStream);
-};
+struct Cartoon;
+void CartoonInput(Cartoon& _cartoon, std::ifstream &_inputStream);
+void CartoonOutput(Cartoon& _cartoon, std::ofstream &_outputStream);
 
-class Cartoon : public Movie {
-private:
-	enum class typeOfCartoon {
-		anime,
-		animation,
-		puppets,
-		plasticine,
-		sandy
-	};
-	typeOfCartoon _creationMethod;
-public:
-	Cartoon();
-	~Cartoon();
-	void input(std::ifstream& _inputStream);
-	void output(std::ofstream& _outputStream);
-};
+struct Movie;
+Movie* MovieInput(std::ifstream& _inputStream);
+void MovieOutput(Movie& _movie, std::ofstream& _outputStream);
+unsigned long long int _countOfVowels(Movie _movie);
 
-class Container {
-public:
-	Container();
-	~Container();
-	void input(std::ifstream& _inputStream);
-	void pushback(Movie* _inputMovie);
-	void print(std::ofstream& _outputStream);
-	void clear();
-private:
-	class ContainerNode {
-	public:
-		ContainerNode();
-		~ContainerNode();
-		Movie* _data;
-		ContainerNode* _next;
-		ContainerNode* _prev;
-	};
-	ContainerNode* _head;
-};
+struct ContainerNode;
+ContainerNode* ContainerNodeInput(std::ifstream& _inputStream);
+void ContainerNodeOutput(ContainerNode* _tempNode, std::ofstream& _outputStream);
+
+struct Container;
+Container* InitializationContainer();
+void ContainerClear(Container* _container);
+bool checkSort(ContainerNode* _first, ContainerNode* _second);
+void ContainerSort(Container* _container);
+void ContainerInput(Container* _container, std::ifstream& _inputStream);
+void ContainerOutput(Container* _container, std::ofstream& _outputStream);
+
+
 #endif // !__MODULES__
 
